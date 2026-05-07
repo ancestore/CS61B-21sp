11b15f4af09136c86d0b3a6537f0a2ba38c60d10
ancestore <3071029675pjh@gmail.com>
2026-05-07 10:39:00 +0800

    Fix logic: use modulo to check for even numbers

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -5,7 +5,7 @@ public class Collatz {
 
     /** Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
-      if(n==2){
+      if(n%2==0){
           return n/2;
       }
       else{
